48c400f16e9df199c2e3e5e8967b2e92e4ec95cd
joaobrambati <joaobrambati@gmail.com>
2025-11-17 01:31:25 -0300

    feat: configura banco de dados

---
+++ backend/Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Show> Shows { get; set; }
+    public DbSet<Banda> Bandas { get; set; }
+    public DbSet<Amigo> Amigos { get; set; }
+}
