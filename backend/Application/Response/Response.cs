eb7a95a727eae48c13b30a78c07dd0833f3a68a5
joaobrambati <joaobrambati@gmail.com>
2025-11-17 01:32:20 -0300

    feat: cria modelo de resposta

---
+++ backend/Application/Response/Response.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Response;
+
+public class Response<T>
+{
+    public T? Data { get; set; }
+    public bool Status { get; set; }
+    public required string Mensagem { get; set; }
+}
