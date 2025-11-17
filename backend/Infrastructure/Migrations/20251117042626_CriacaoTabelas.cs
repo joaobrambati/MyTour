389ebd1848e2e2bddb577df900c65c7a96d25f86
joaobrambati <joaobrambati@gmail.com>
2025-11-17 01:31:55 -0300

    feat: migration com criação das tabelas

---
+++ backend/Infrastructure/Migrations/20251117042626_CriacaoTabelas.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriacaoTabelas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Amigos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ShowId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Amigos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Bandas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ShowId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bandas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Shows",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BandaPrincipalId = table.Column<int>(type: "int", nullable: false),
+                    Data = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Genero = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ValorIngresso = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    Avaliacao = table.Column<int>(type: "int", nullable: false),
+                    Observacao = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Shows", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Shows_Bandas_BandaPrincipalId",
+                        column: x => x.BandaPrincipalId,
+                        principalTable: "Bandas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Amigos_ShowId",
+                table: "Amigos",
+                column: "ShowId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bandas_ShowId",
+                table: "Bandas",
+                column: "ShowId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Shows_BandaPrincipalId",
+                table: "Shows",
+                column: "BandaPrincipalId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Amigos_Shows_ShowId",
+                table: "Amigos",
+                column: "ShowId",
+                principalTable: "Shows",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bandas_Shows_ShowId",
+                table: "Bandas",
+                column: "ShowId",
+                principalTable: "Shows",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bandas_Shows_ShowId",
+                table: "Bandas");
+
+            migrationBuilder.DropTable(
+                name: "Amigos");
+
+            migrationBuilder.DropTable(
+                name: "Shows");
+
+            migrationBuilder.DropTable(
+                name: "Bandas");
+        }
+    }
+}
