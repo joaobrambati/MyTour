7da3a08990437d62e6bebe126854dcd3dca4d76a
joaobrambati <joaobrambati@gmail.com>
2025-11-21 11:37:37 -0300

    feat: declara repositorio de Amigo

---
+++ backend/Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Infrastructure.Repositories.Implementations;
+using Infrastructure.Repositories.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructureDI(this IServiceCollection services)
+    {
+        services.AddScoped<IAmigoRepository, AmigoRepository>();
+
+        return services;
+    }
+}
