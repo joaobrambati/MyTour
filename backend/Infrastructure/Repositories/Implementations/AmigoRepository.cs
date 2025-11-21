36f30aa7bfe7ce5f2bd545905cc6a97c17661766
joaobrambati <joaobrambati@gmail.com>
2025-11-21 11:39:33 -0300

    feat: cria repositorio e sua implementação de Amigo

---
+++ backend/Infrastructure/Repositories/Implementations/AmigoRepository.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Entities;
+using Infrastructure.Data;
+using Infrastructure.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.Implementations;
+public class AmigoRepository : IAmigoRepository
+{
+    private readonly AppDbContext _context;
+
+    public AmigoRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Amigo>> GetAll()
+        => await _context.Amigos.ToListAsync();
+
+    public async Task<Amigo?> GetById(int id)
+        => await _context.Amigos.FindAsync(id);
+
+    public async Task Create(Amigo amigo)
+        => await _context.Amigos.AddAsync(amigo);
+
+    public void Update(Amigo amigo)
+        => _context.Amigos.Update(amigo);
+
+    public void Delete(Amigo amigo)
+        => _context.Amigos.Remove(amigo);
+
+    public async Task SaveChangesAsync()
+        => await _context.SaveChangesAsync();
+
+}
---
+++ backend/Infrastructure/Repositories/Interfaces/IAmigoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+
+namespace Infrastructure.Repositories.Interfaces;
+
+public interface IAmigoRepository
+{
+    Task<List<Amigo>> GetAll();
+    Task<Amigo?> GetById(int id);
+    Task Create(Amigo amigo);
+    void Update(Amigo amigo);
+    void Delete(Amigo amigo);
+    Task SaveChangesAsync();
+}
