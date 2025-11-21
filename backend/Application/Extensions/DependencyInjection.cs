37ef57388c22609d5cd66055f151bfbf0dfd052e
joaobrambati <joaobrambati@gmail.com>
2025-11-21 11:37:18 -0300

    feat: declara service de Amigo

---
+++ backend/Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿using Application.Interfaces;
+using Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application.Extensions;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplicationDI(this IServiceCollection services)
+    {
+        services.AddScoped<IAmigoService, AmigoService>();
+
+        return services;
+    }
+}
